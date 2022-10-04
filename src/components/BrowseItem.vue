ef74bb62827a53d4d62beccdc0209163cebf604b
jtreeves <jackson.t.reeves@gmail.com>
2022-10-03 21:23:18 -0400

    [BrowseItem] Add button to add to cart; conditionally render warnings about stock

--- src/components/BrowseItem.vue
+++ src/components/BrowseItem.vue
@@ -3,11 +3,29 @@
         <p>{{ product.name }}</p>
         <p>{{ product.image }}</p>
         <p>${{ formatPrice(product.price) }}</p>
+        
+        <p v-if="tally < 6 && tally > 0">
+            Only {{ tally }} left in stock!
+        </p>
+
+        <button 
+            v-if="tally > 0"
+            @click="addProductToCart"
+        >
+            Add Product to Cart
+        </button>
+
+        <p v-if="tally === 0">
+            OUT OF STOCK!
+        </p>
     </li>
 </template>
 
 <script setup lang="ts">
+    import { computed, ComputedRef } from 'vue'
     import { Product } from '@/interfaces'
+    import cart from '@/store/cart'
+    import stock from '@/store/stock'
     import findProductById from '@/utilities/findProductById'
     import formatPrice from '@/utilities/formatPrice'
 
@@ -15,7 +33,16 @@
         id: string
     }>()
 
+    const tally: ComputedRef<number> = computed(() => {
+        return stock.findProduct(props.id).tally
+    })
+
     const product: Product = findProductById(props.id)
+
+    function addProductToCart(): void {
+        cart.addProduct(props.id)
+        stock.removeProduct(props.id)
+    }
 </script>
 
 <style scoped>
