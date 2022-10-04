be23835bd549243abd9c0f06d066e203ab927ed0
jtreeves <jackson.t.reeves@gmail.com>
2022-10-03 21:02:48 -0400

    [BrowsePage] Display category; change products based on category

--- src/views/BrowsePage.vue
+++ src/views/BrowsePage.vue
@@ -1,14 +1,38 @@
 <template>
-    <h1>Results</h1>
+    <h1>{{ selectedCategory.name }}</h1>
+
+    <select 
+        v-model="selectedCategory" 
+        @change="changeProducts"
+    >
+        <option 
+            v-for="category in categories" 
+            :value="category" 
+            :key="category.name"
+        >
+            {{ category.name }}
+        </option>
+    </select>
 
     <BrowseList 
-        :products="stock.value"
+        :products="matchingProducts"
     />
 </template>
 
 <script setup lang="ts">
+    import { ref, Ref } from 'vue'
+    import { Category, ProductTally } from '@/interfaces'
+    import categories from '@/data/categories'
     import stock from '@/store/stock'
     import BrowseList from '@/components/BrowseList.vue'
+    import findProductTalliesByCategory from '@/utilities/findProductTalliesByCategory'
+
+    const selectedCategory: Ref<Category> = ref(categories[0])
+    const matchingProducts: Ref<ProductTally[]> = ref(stock.value)
+
+    function changeProducts(): void {
+        matchingProducts.value = findProductTalliesByCategory(selectedCategory.value)
+    }
 </script>
 
 <style scoped>
